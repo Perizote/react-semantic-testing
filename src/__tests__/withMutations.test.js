3bc50ba8466955979bd2029d1e9e2b7804695043
Perizote <sergio.perizote@gmail.com>
2019-04-05 20:48:21 +0200

    [Changed] improve withMutation tests readability

--- src/__tests__/withMutations.test.js
+++ src/__tests__/withMutations.test.js
@@ -3,29 +3,34 @@ import { mount } from './mount.util'
 describe('withMutations', () => {
   const initialText = 'an initial text'
   const node = `<span>${ initialText }</span>`
+  let mountedNodeWithTools
+
+  beforeEach(() => {
+    mountedNodeWithTools = mount(node)
+  })
+
+  afterEach(() => {
+    mountedNodeWithTools.unmount()
+  })
 
   async function testThatObserverThrowsError(observerName, errorMessage) {
     jest.useFakeTimers()
-    const mountedNodeWithTools = mount(node)
     const observer = mountedNodeWithTools[observerName]()
+
     jest.advanceTimersByTime(3100)
 
     await expect(observer).rejects.toBe(errorMessage)
-
-    mountedNodeWithTools.unmount()
     jest.useRealTimers()
   }
 
   it('should observe changes', async () => {
     const changedText = 'a changed text'
-    const mountedNodeWithTools = mount(node)
     const asyncChange = () => setTimeout(() => mountedNodeWithTools.getRawNode().textContent = changedText)
     expect(mountedNodeWithTools.getText()).toBe(initialText)
 
     asyncChange()
 
     expect((await mountedNodeWithTools.willChange()).getText()).toBe(changedText)
-    mountedNodeWithTools.unmount()
   })
 
   it('should throw an error when nothing changes', () => {
@@ -33,14 +38,12 @@ describe('withMutations', () => {
   })
 
   it('should observe new renders', async () => {
-    const mountedNodeWithTools = mount(node)
     const asyncRender = () => setTimeout(() => mountedNodeWithTools.getRawNode().innerHTML = '<p>another node</p>')
     expect(mountedNodeWithTools.getByText('another node').isRendered()).toBeFalsy()
 
     asyncRender()
 
     expect((await mountedNodeWithTools.getByText('another node').willRender()).isRendered()).toBeTruthy()
-    mountedNodeWithTools.unmount()
   })
 
   it('should throw an error when nothing new is rendered', () => {
@@ -48,14 +51,12 @@ describe('withMutations', () => {
   })
 
   it('should observe disappearances', async () => {
-    const mountedNodeWithTools = mount(node)
     const asyncDisappear = () => setTimeout(() => mountedNodeWithTools.getRawNode().innerHTML = null)
     expect(mountedNodeWithTools.getByText(initialText).isRendered()).toBeTruthy()
 
     asyncDisappear()
 
     expect((await mountedNodeWithTools.getByText(initialText).willDisappear()).isRendered()).toBeFalsy()
-    mountedNodeWithTools.unmount()
   })
 
   it('should throw an error when nothing disappears', () => {
